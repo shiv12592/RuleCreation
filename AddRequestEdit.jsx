0e0de3ea58825ff7c3b08781414b2a4e41ee7ccc
Shiv Jayswal <36148147+shiv12592@users.noreply.github.com>
2024-02-27 00:37:02 +0530

    Update AddRequestEdit.jsx

--- AddRequestEdit.jsx
+++ AddRequestEdit.jsx
@@ -1,34 +1,107 @@
+// Parent.js
+import React, { Component } from "react";
+import { reduxForm } from "redux-form";
+import AddRequest from "./AddRequest";
+import RuleCategoryChangeEditApproval from "./RuleCategoryChangeEditApproval";
+import { Container, Row } from "react-bootstrap";
+
+export class Parent extends Component {
+  static propTypes = {
+    ruleDetails: PropTypes.object.isRequired,
+  };
+  static defaultProps = {};
+  state = {
+    ruleDescription: "",
+    errorMessage: null,
+    newCategory: "",
+    newCarId: "",
+    newRuleOwner: "",
+    requestData: [], // initialize the state with an empty array
+  };
+  constructor(props) {
+    super(props);
+    this.handleRequestData = this.handleRequestData.bind(this); // bind the handleRequestData method
+  }
+
+  componentDidMount() {
+    const { ruleDetails } = this.props;
+    const ruleCategory = ruleDetails.category;
+    const carId = ruleDetails.carId;
+    const ruleOwner = ruleDetails.owner;
+    this.setState({
+      newCategory: ruleCategory,
+      newCarId: carId,
+      newRuleOwner: ruleOwner,
+      requestData: ruleDetails.requestData, // set the initial requestData from the props
+    });
+  }
+
+  handleCategoryChange = (updatedValues) => {
+    this.setState(updatedValues);
+  };
+
+  handleRequestData = (updatedList) => {
+    // this method will receive the updated list from the AddRequest component
+    this.setState({ requestData: updatedList }); // update the state with the new list
+  };
+
+  render() {
+    const { formFields } = this.props;
+    const { newCategory, newCarId, newRuleOwner, requestData } = this.state;
+    return (
+      <div>
+        <Container className="col-md-12 pad-1 card-rounded margin-1-b">
+          <Row>
+            <AddRequest
+              onChange={this.handleRequestData}
+              requestData={requestData} // pass the requestData state as props
+            />
+            <RuleCategoryChangeEditApproval
+              category={newCategory}
+              carId={newCarId}
+              ruleOwner={newRuleOwner}
+              onChange={this.handleCategoryChange}
+            />
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+export const EditRule = reduxForm(formOptions)(Parent);
+
 // AddRequest.js
 import React, { useState } from "react";
 
-export default function AddRequest({ onChange }) {
-  // initialize the state with an empty array
-  const [requestData, setRequestData] = useState([]);
+export default function AddRequest({ onChange, requestData }) {
+  // use the requestData prop to initialize the state
+  const [requestList, setRequestList] = useState(requestData);
 
   // handle the change of the select and input fields
   const handleChange = (e, index) => {
     const { name, value } = e.target;
-    const list = [...requestData];
+    const list = [...requestList];
     list[index][name] = value;
-    setRequestData(list);
+    setRequestList(list);
     onChange(list); // pass the updated list to the parent component
   };
 
   // handle the click of the add request button
   const handleAdd = () => {
     // add a new object with empty values to the state array
-    setRequestData([
-      ...requestData,
+    setRequestList([
+      ...requestList,
       { attribute: "", operation: "", value: "" },
     ]);
+    onChange(requestList); // pass the updated list to the parent component
   };
 
   // handle the click of the remove request button
   const handleRemove = (index) => {
     // remove the object at the given index from the state array
-    const list = [...requestData];
+    const list = [...requestList];
     list.splice(index, 1);
-    setRequestData(list);
+    setRequestList(list);
     onChange(list); // pass the updated list to the parent component
   };
 
@@ -36,7 +109,7 @@ export default function AddRequest({ onChange }) {
   return (
     <div className="AddRequest">
       <h3>Request Form</h3>
-      {requestData.map((item, i) => (
+      {requestList.map((item, i) => (
         <div key={i}>
           <select
             name="attribute"
@@ -68,7 +141,7 @@ export default function AddRequest({ onChange }) {
               placeholder="Enter a value"
             />
           )}
-          {requestData.length > 1 && ( // only show the remove button if there are more than one rows
+          {requestList.length > 1 && ( // only show the remove button if there are more than one rows
             <button onClick={() => handleRemove(i)}>Remove Request</button>
           )}
         </div>
@@ -77,73 +150,3 @@ export default function AddRequest({ onChange }) {
     </div>
   );
 }
-
-// EditRulePlain.js
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { reduxForm } from "redux-form";
-import AddRequest from "./AddRequest";
-import RuleCategoryChangeEditApproval from "./RuleCategoryChangeEditApproval";
-import { Container, Row } from "react-bootstrap";
-
-export class EditRulePlain extends Component {
-  static propTypes = {
-    ruleDetails: PropTypes.object.isRequired,
-  };
-  static defaultProps = {};
-  state = {
-    ruleDescription: "",
-    errorMessage: null,
-    newCategory: "",
-    newCarId: "",
-    newRuleOwner: "",
-    requestData: [],
-  };
-  constructor(props) {
-    super(props);
-    this.handleRequestData = this.handleRequestData.bind(this); // bind the handleRequestData method
-  }
-
-  componentDidMount() {
-    const { ruleDetails } = this.props;
-    const ruleCategory = ruleDetails.category;
-    const carId = ruleDetails.carId;
-    const ruleOwner = ruleDetails.owner;
-    this.setState({
-      newCategory: ruleCategory,
-      newCarId: carId,
-      newRuleOwner: ruleOwner,
-      requestData: ruleDetails.requestData, // set the initial requestData from the props
-    });
-  }
-
-  handleCategoryChange = (updatedValues) => {
-    this.setState(updatedValues);
-  };
-
-  handleRequestData = (updatedList) => {
-    // this method will receive the updated list from the AddRequest component
-    this.setState({ requestData: updatedList }); // update the state with the new list
-  };
-
-  render() {
-    const { formFields } = this.props;
-    const { newCategory, newCarId, newRuleOwner } = this.state;
-    return (
-      <div>
-        <Container className="col-md-12 pad-1 card-rounded margin-1-b">
-          <Row>
-            <AddRequest onChange={this.handleRequestData} />
-            <RuleCategoryChangeEditApproval
-              category={newCategory}
-              carId={newCarId}
-              ruleOwner={newRuleOwner}
-              onChange={this.handleCategoryChange}
-            />
-          </Row>
-        </Container>
-      </div>
-    );
-  }
-}
-export const EditRule = reduxForm(formOptions)(EditRulePlain);
