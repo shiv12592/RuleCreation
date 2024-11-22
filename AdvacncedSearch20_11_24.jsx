06879ccd042ccd6c0b92ee4c7516fb3a252e3047
Shiv Jayswal <36148147+shiv12592@users.noreply.github.com>
2024-11-22 17:08:11 +0530

    Update AdvacncedSearch22_11_24.jsx

--- AdvacncedSearch20_11_24.jsx
+++ AdvacncedSearch20_11_24.jsx
@@ -3,12 +3,13 @@ import PropTypes from "prop-types";
 
 const RulesListTable = ({
   ruleSearching,
-  handLeChangeSearch,
   handleChangeFilter,
   searchEnabled,
   filter,
 }) => {
-  const [advancedFilters, setAdvancedFilters] = useState([]);
+  const [advancedFilters, setAdvancedFilters] = useState([
+    { field: "", operator: "", value: "" },
+  ]);
   const [isAdvancedSearch, setIsAdvancedSearch] = useState(false);
 
   const fieldOptions = [
@@ -38,25 +39,33 @@ const RulesListTable = ({
 
   const addNewFilter = () => {
     setAdvancedFilters([...advancedFilters, { field: "", operator: "", value: "" }]);
-    setIsAdvancedSearch(true);
   };
 
   const handleFilterChange = (index, key, value) => {
     const updatedFilters = [...advancedFilters];
     updatedFilters[index][key] = value;
     setAdvancedFilters(updatedFilters);
+
+    // Call handleChangeFilter to propagate filter updates to the parent
     handleChangeFilter(JSON.stringify(updatedFilters));
   };
 
   const removeFilter = (index) => {
     const updatedFilters = advancedFilters.filter((_, i) => i !== index);
     setAdvancedFilters(updatedFilters);
+   // Propagate the updated filters to the parent
     handleChangeFilter(JSON.stringify(updatedFilters));
     if (updatedFilters.length === 0) setIsAdvancedSearch(false);
   };
 
+  const handleSearch = () => {
+    // Call handleChangeSearch to trigger the search API
+    handleChangeSearch(JSON.stringify(advancedFilters));
+  };
+
   return (
     <div>
+      {/* Basic Search Input */}
       {!isAdvancedSearch && (
         <div>
           <input
@@ -66,102 +75,241 @@ const RulesListTable = ({
             onChange={(e) => handleChangeFilter(e.target.value)}
             disabled={ruleSearching || isAdvancedSearch}
           />
-        </div>
-      )}
-
-      <div>
-        {isAdvancedSearch && (
-          <div>
-            {advancedFilters.map((filter, index) => (
-              <div key={index} style={{ display: "flex", marginBottom: "10px" }}>
-                {/* Field Dropdown */}
-                <select
-                  value={filter.field}
-                  onChange={(e) => handleFilterChange(index, "field", e.target.value)}
-                >
-                  <option value="">Select Field</option>
-                  {fieldOptions.map((opt) => (
-                    <option key={opt.value} value={opt.value}>
-                      {opt.label}
-                    </option>
-                  ))}
-                </select>
-
-                {/* Operator Dropdown */}
-                <select
-                  value={filter.operator}
-                  onChange={(e) => handleFilterChange(index, "operator", e.target.value)}
-                  style={{ marginLeft: "10px" }}
-                >
-                  <option value="">Select Operator</option>
-                  {operatorOptions.map((opt) => (
-                    <option key={opt.value} value={opt.value}>
-                      {opt.label}
-                    </option>
-                  ))}
-                </select>
-
-                {/* Value Input/Dropdown */}
-                {filter.field === "Category" || filter.field === "State" ? (
-                  <select
-                    value={filter.value}
-                    onChange={(e) => handleFilterChange(index, "value", e.target.value)}
-                    style={{ marginLeft: "10px" }}
-                  >
-                    <option value="">Select Value</option>
-                    {valueOptions[filter.field]?.map((opt) => (
-                      <option key={opt.value} value={opt.value}>
-                        {opt.label}
-                      </option>
-                    ))}
-                  </select>
-                ) : (
-                  <input
-                    type="text"
-                    value={filter.value}
-                    placeholder="Enter Value"
-                    onChange={(e) => handleFilterChange(index, "value", e.target.value)}
-                    style={{ marginLeft: "10px" }}
-                  />
-                )}
-
-                {/* Remove Button */}
-                <button
-                  onClick={() => removeFilter(index)}
-                  style={{ marginLeft: "10px", color: "red" }}
-                >
-                  Remove
-                </button>
-              </div>
-            ))}
-
-            <button onClick={addNewFilter} style={{ marginTop: "10px" }}>
-              Add New Filter
-            </button>
-          </div>
-        )}
-
-        {!isAdvancedSearch && (
           <button
             onClick={() => {
               setIsAdvancedSearch(true);
-              setAdvancedFilters([]);
             }}
-            style={{ marginTop: "10px", backgroundColor: "green", color: "white" }}
+            style={{
+              marginLeft: "10px",
+              backgroundColor: "green",
+              color: "white",
+              padding: "5px 10px",
+              border: "none",
+              cursor: "pointer",
+            }}
           >
             Advanced Search
           </button>
-        )}
-      </div>
+        </div>
+      )}
+
+      {/* Advanced Search Section */}
+      {isAdvancedSearch && (
+        <div>
+      {advancedFilters.map((currentFilter, index) => (
+            <div key={index} style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}>
+              {/* Field Dropdown */}
+              <select
+            value={currentFilter.field}
+                onChange={(e) => handleFilterChange(index, "field", e.target.value)}
+                style={{ marginRight: "10px" }}
+              >
+                <option value="">Select Field</option>
+                {fieldOptions.map((opt) => (
+                  <option key={opt.value} value={opt.value}>
+                    {opt.label}
+                  </option>
+                ))}
+              </select>
+
+              {/* Operator Dropdown */}
+              <select
+            value={currentFilter.operator}
+                onChange={(e) => handleFilterChange(index, "operator", e.target.value)}
+                style={{ marginRight: "10px" }}
+              >
+                <option value="">Select Operator</option>
+                {operatorOptions.map((opt) => (
+                  <option key={opt.value} value={opt.value}>
+                    {opt.label}
+                  </option>
+                ))}
+              </select>
+
+              {/* Value Input/Dropdown */}
+          {currentFilter.field === "Category" || currentFilter.field === "State" ? (
+                <select
+              value={currentFilter.value}
+                  onChange={(e) => handleFilterChange(index, "value", e.target.value)}
+                  style={{ marginRight: "10px" }}
+                >
+                  <option value="">Select Value</option>
+              {valueOptions[currentFilter.field]?.map((opt) => (
+                    <option key={opt.value} value={opt.value}>
+                      {opt.label}
+                    </option>
+                  ))}
+                </select>
+              ) : (
+                <input
+                  type="text"
+              value={currentFilter.value}
+                  placeholder="Enter Value"
+                  onChange={(e) => handleFilterChange(index, "value", e.target.value)}
+                  style={{ marginRight: "10px" }}
+                />
+              )}
+
+              {/* Remove Button */}
+              <button
+                onClick={() => removeFilter(index)}
+                style={{
+                  marginRight: "10px",
+                  backgroundColor: "red",
+                  color: "white",
+                  padding: "5px 10px",
+                  border: "none",
+                  cursor: "pointer",
+                }}
+              >
+                Remove
+              </button>
+
+              {/* Add New Filter Button */}
+              {index === advancedFilters.length - 1 && (
+                <button
+                  onClick={addNewFilter}
+                  style={{
+                    backgroundColor: "blue",
+                    color: "white",
+                    padding: "5px 10px",
+                    border: "none",
+                    cursor: "pointer",
+                  }}
+                >
+                  Add New Filter
+                </button>
+              )}
+            </div>
+          ))}
+
+          {/* Search Button */}
+          <button
+            onClick={handleSearch}
+            style={{
+              marginTop: "10px",
+              backgroundColor: "green",
+              color: "white",
+              padding: "10px 20px",
+              border: "none",
+              cursor: "pointer",
+            }}
+          >
+            Search
+          </button>
+        </div>
+      )}
     </div>
   );
 };
 
 RulesListTable.propTypes = {
-  handLeChangeSearch: PropTypes.func.isRequired,
+  handleChangeSearch: PropTypes.func.isRequired,
   handleChangeFilter: PropTypes.func.isRequired,
   ruleSearching: PropTypes.bool.isRequired,
   searchEnabled: PropTypes.bool.isRequired,
+  filter: PropTypes.string.isRequired,
 };
 
 export default RulesListTable;
+
+
+
+
+
+//old code used for edit
+
+import { useState } from "react";
+
+const PropTypes = require("prop-types");
+
+// parent file which calles the RulesListTable, RulesListTable sent back the filter value to seach teh rule list by that text,
+// now need to do avcanced search as per Image provided where after click on Add New Filter Row will add with three Columns Field, Operator and Value.
+// Field should be drop down which contains the Rule no, State, Name, Owner, Category
+// operator should have Equals and not Equals dropdown values
+// vallue column should contain TextBox for Rule No, Owner , Name and DropDown for CAtegory (Application, Organizational), state(Active, Disabled Inactive)
+// do the Ui coding under RulesListTable where AdvancedSearch TAG is mentioned
+// when Add New filter selected make searchInput  disable and enable AdvancedSearch,
+// make json of all filtred data and pass as string to filter, pass the filter, ruleSearching, handleChangeSearch and handleChnageFilter values back to parent component from AdvancedSerarch tag
+const Home = () =>{
+
+    const [filter, setFilter] = useState("");
+    const [searchEnabled, setSearchEnabled] = useState(false);
+    const [ruleSearching, setRuleSearching] = useState(false);
+
+    const handleChangeSearch = async ()  => {
+        setSearchEnabled(  true);
+        setRuleSearching(  true);
+        if (filter) {
+        dispatchLoadCurrentAppsSearchTxt(filter);
+        const status = await dispatchSearchHomeAppsList(filter, 1, PAGE_SIZE, role, true);
+        if (status) {
+          setState(  prevState => ({
+        ... prevState,
+        currentPage: 0,
+          }));
+        setCurrentPageBackup(state.currentPage);
+        setRuleSearching(  false);
+          }
+        }
+    };
+
+        const handleChangeFilter = (inFilter)  => {
+        setFilter(inFilter);
+        if (!inFilter) {
+        setSearchEnabled( false);
+        setState(  prevState  => ({
+        ... prevState,
+        currentPage: currentPageBackup,
+        }));
+        dispatchLoadCurrentAppsSearchTxt(null);
+        }
+    };
+
+    // other code
+return (
+    <PageWrapper>
+        {/* other code  */}
+            <RulesListTable
+                rulesList= {rulesList}
+                total={allRules.total}
+                currentPage={state.currentPage}
+                handLePageChange={handLePageChange}
+                filter={filter}
+                handLeChangeSearch={handLeChangeSearch}
+                handleChangeFilter ={handleChangeFilter }
+                ruleSearching={ruleSearching}
+                searchEnabled={searchEnabled}
+            />
+            {/* other code  */}
+     </PageWrapper>
+
+)
+};
+
+
+const RulesListTable = () =>{
+    const {
+        ruleSearching, handLeChangeSearch, handLePageChange, handleChangeFilter, filter,
+    } = props;
+
+    return(
+        <div>
+                <SearchInput>
+                    type="text",
+                    placeholder="Search Rule",
+                    value={filter},
+                    onSubmit={handleChangeSearch},
+                    onChange={handleChangeFilter},
+                    searching={ruleSearching}
+                </SearchInput>
+        </div>
+    )
+}
+RulesListTable.prototype = {
+    handLeChangeSearch: PropTypes.func.isRequired,
+    handleChangeFilter: PropTypes.func.isRequired,
+    ruleSearching: PropTypes.bool.isRequired,
+    searchEnabled: PropTypes.bool.isRequired,
+    filter: PropTypes.string.isRequired,
+}
